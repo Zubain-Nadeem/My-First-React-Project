39db39b641e00dcf608d4304de0b529058c56fd0
Zubainali23@gmail.com <zubainali23@gmail.com>
2025-05-22 20:14:32 +0500

    Header & Hero Section Completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+
+const Header = () => {
+  return (
+    <div>
+      <header className="w-full bg-[#008D38] ">
+        <div className="flex items-center w-[90%] m-auto justify-between h-[64px] text-white">
+        <h1 className=" text-2xl font-bold">Green Protocol</h1>
+        
+            <ul className="flex gap-5 text-lg ">
+                <li><a href="#">Problem</a></li>
+                <li><a href="#">Solution</a></li>
+                <li><a href="#">Business Model</a></li>
+                <li><a href="#">Profit Distribution</a></li>
+            </ul>
+    
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
+
---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import image1 from "../assets/Hero-Section-images/hero-image-1.png";
+import image2 from "../assets/Hero-Section-images/hero-image-2.png";
+import image3 from "../assets/Hero-Section-images/hero-image-3.png";
+import dotimage from "../assets/Hero-Section-images/dot-image.png";
+
+const Hero = () => {
+  return (
+    <div>
+      <section className="h-[78vh] w-full bg-[#00AE47] flex justify-center items-center ">
+        <div>
+          <h2 className=" mb-[70px] text-[50px] text-center font-bold text-white">
+            Green Protocol
+          </h2>
+          <div className="flex items-center gap-5">
+            <img
+              className="rounded-[100px] shadow-[0_0_10px_#007000] h-[160px] w-[160px]"
+              src={image1}
+              alt=""
+            />
+            <img className="w-[100px]" src={dotimage} alt="" />
+            <img  className="rounded-[100px] shadow-[0_0_10px_#007000] h-[160px] w-[160px]" src={image2} alt="" />
+            <img className="w-[100px]" src={dotimage} alt="" />
+            <img className="rounded-[100px] shadow-[0_0_10px_#007000] h-[160px] w-[160px]" src={image3} alt="" />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Hero;
